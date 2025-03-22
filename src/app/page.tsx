8f04058f1a458494f24462eb21b2869c5e53dd8c
Ayush Bangar <143717997+HeatherLead@users.noreply.github.com>
2025-03-23 01:37:45 +0530

    ui changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,14 @@
 "use client";
-import { useEffect } from "react";
 import Spline from "@splinetool/react-spline";
 import { Suspense } from "react";
-import { useAuth } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
 
 export default function RootPage() {
-  const userId = useAuth();
-  useEffect(() => {
-    if (userId) {
-      redirect("/chat");
-    }
-  }, []);
   return (
     <main className="flex min-h-screen overflow-x-hidden  flex-col items-center justify-between bg-black">
       <Suspense fallback={<div className="text-white">Loading...</div>}>
         <main className="w-screen h-screen z-20 overflow-hidden">
           <Spline
-            scene="https://prod.spline.design/azAhVF9QLiK-9oJ0/scene.splinecode"
+            scene="https://prod.spline.design/ZP27cyW1kbOob5QI/scene.splinecode"
             onError={(error) => console.log("Spline error:", error)}
           />
         </main>
