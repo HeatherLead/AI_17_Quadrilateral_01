2adb340b86f3e9413c4c8a6447a8b86b3d606a70
HemantJatal <hemantjatal59@gmail.com>
2025-03-23 00:58:34 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,14 @@ export default function RootPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-black">
       <Suspense fallback={<div className="text-white">Loading...</div>}>
-        <main className="w-screen h-screen z-20">
+        <div className="w-screen h-screen z-20">
+          {" "}
+          {/* Changed from <main> to <div> */}
           <Spline
             scene="https://prod.spline.design/ZP27cyW1kbOob5QI/scene.splinecode"
             onError={(error) => console.log("Spline error:", error)}
           />
-        </main>
+        </div>
       </Suspense>
     </main>
   );
